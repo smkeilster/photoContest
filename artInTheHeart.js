147ca1ecb659f910e3c95dddda5dcfde6bff42eb
smkeilster <36768294+smkeilster@users.noreply.github.com>
2018-08-19 18:51:38 -0500

    Still working on sort features

--- artInTheHeart.js
+++ artInTheHeart.js
@@ -1,7 +1,6 @@
 let xhr = new XMLHttpRequest();
 
 
-
 //get function from the database
 function getSelfies() {
     xhr.open('GET', "https://selfieproject.azurewebsites.net/api/selfie", true);
@@ -13,12 +12,43 @@ function getSelfies() {
 function processRequest(e) {
     if (xhr.readyState == 4 && xhr.status == 200) {
         let entries = JSON.parse(xhr.responseText);
-        console.log(entries);
+        // console.log(entries);
         buildLeaderboard(entries);
         buildGallery(entries);
+        buildArrays(entries);
     }
 }
 
+// function project(){
+
+// };
+
+// function date(){
+
+// };
+
+function buildArrays(entries){
+    let mostVotes = entries.sort((a,b) => b.voteTotal-a.voteTotal);
+    let mostRecent = entries.sort((a,b) => new Date(b.dateTaken)-new Date(a.dateTaken));
+}
+   
+function sortBy(){
+    var x = document.getElementById("sort").value;
+    console.log(x);
+    if(x=="votes"){
+           
+    document.getElementById("gallery").innerHTML="";
+    buildGallery(mostVotes)
+    }
+}
+
+
+
+
+
+
+
+// builds the leaderboard for top 5 votes
 function buildLeaderboard(entries) {
     // variables that will be used in multiple statements
     let builtLeaderboard = [];
@@ -45,7 +75,7 @@ function buildLeaderboard(entries) {
         // adds the image to the entry div
         entry.appendChild(image);
         // pulls in the photo id from the database
-        let photoId = entries[i].imageId;
+        let photoId = entries[i].selfieId;
         // creates the element for the cityState
         let ID = document.createElement("h5");
         // creates the text within the element
@@ -63,17 +93,17 @@ function buildLeaderboard(entries) {
         voteTotal.appendChild(textnode2);
         entry.appendChild(voteTotal);
     }
-  }    
+  };    
   
-  
-  function buildGallery(entries) {
+// builds the gallery for the remaining images based on most recent  
+function buildGallery(entries) {
     // variables that will be used in multiple statements
     let builtGallery = [];
-    const sortedDate = entries.sort((a,b) => new Date(b.dateTaken)-new Date(a.dateTaken));
-    console.log(sortedDate);
+    // const sortedDate = entries.sort((a,b) => new Date(b.dateTaken)-new Date(a.dateTaken));
+    // console.log(sortedDate);
     for (var i = 0; i <entries.length; i++) {
         // creates the span using the unique anchorLink column as the id.
-        entryId = entries[i].ImageId;
+        entryId = entries[i].imageId;
         // creates entry
         entry = document.createElement("div");
         // assigns id for the entry
@@ -91,7 +121,7 @@ function buildLeaderboard(entries) {
         // adds the image to the entry div
         entry.appendChild(image);
         // pulls in the photo id from the database
-        let photoId = entries[i].imageId;
+        let photoId = entries[i].selfieId;
         // creates the element for the cityState
         let ID = document.createElement("h5");
         // creates the text within the element
@@ -108,23 +138,29 @@ function buildLeaderboard(entries) {
         // these three lines nest the variables within the new header in the imageDiv
         voteTotal.appendChild(textnode2);
         entry.appendChild(voteTotal);
+    } 
+};    
+  
+//  shrinks header on scroll so phone numberis always visible 
+window.onscroll = function (){shrinkHeader()};
+
+let header = document.getElementById("header");
+let other = document.getElementById("other");
+let offset =header.offsetTop;
+
+function shrinkHeader(){
+    console.log(header.className);
+    if (window.pageYOffset > offset){
+        header.classList.replace("header","smallHeader");
+        other.classList.add("hidden");
     }
-  }      
+    else{
+        header.classList.replace("smallHeader", "header");
+        other.classList.remove("hidden");
+    }
+};
+
+
+
   
-  
-  window.onscroll = function (){shrinkHeader()};
-  
-  let header = document.getElementById("header");
-  let offset =header.offsetTop;
-  
-  function shrinkHeader(){
-  console.log(header.className);
-  if (window.pageYOffset > offset){
-    header.classList.replace("header","smallHeader");
-  }
-  else{
-    header.classList.replace("smallHeader", "header");
-  }
-  
-  };
 getSelfies()
